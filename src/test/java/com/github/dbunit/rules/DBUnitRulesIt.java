19d024d97b75d6e6fd9387b4076fe7c5b366d53d
rmpestano <rmpestano@gmail.com>
2015-07-23 18:39:45 -0300

    perf

--- src/test/java/com/github/dbunit/rules/DBUnitRulesIt.java
+++ src/test/java/com/github/dbunit/rules/DBUnitRulesIt.java
@@ -1,16 +1,15 @@
 package com.github.dbunit.rules;
 
-import com.github.dbunit.rules.dataset.DataSet;
-import com.github.dbunit.rules.jpa.EntityManagerProvider;
-import com.github.dbunit.rules.model.User;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.extractProperty;
+import com.github.dbunit.rules.dataset.DataSet;
+import com.github.dbunit.rules.jpa.EntityManagerProvider;
+import com.github.dbunit.rules.model.User;
 
 /**
  * Created by pestano on 23/07/15.
@@ -25,8 +24,9 @@ public class DBUnitRulesIt {
     @Rule
     public DBUnitRule dbUnitRule = DBUnitRule.instance(emProvider.getConnection());
 
+
     @Test
-    @DataSet(value = "datasets/yml/incomplete-user-dataset.yml",disableConstraints = true)
+    @DataSet(value = "datasets/yml/partial-user-dataset.yml",disableConstraints = true)
     public void shouldSeedDataSetDisablingContraints() {
         User user = (User) emProvider.em().createQuery("select u from User u where u.id = 1").getSingleResult();
         assertThat(user).isNotNull();
@@ -34,7 +34,7 @@ public class DBUnitRulesIt {
     }
 
     @Test
-    @DataSet(value = "datasets/yml/incomplete-user-dataset.yml", executeStatementsBefore = "SET DATABASE REFERENTIAL INTEGRITY FALSE;")
+    @DataSet(value = "datasets/yml/partial-user-dataset.yml", executeStatementsBefore = "SET DATABASE REFERENTIAL INTEGRITY FALSE;")
     public void shouldSeedDataSetDisablingContraintsViaStatement() {
         User user = (User) emProvider.em().createQuery("select u from User u where u.id = 1").getSingleResult();
         assertThat(user).isNotNull();
@@ -42,7 +42,7 @@ public class DBUnitRulesIt {
     }
 
     @Test
-    @DataSet(value = "datasets/yml/incomplete-user-dataset.yml", useSequenceFiltering = true)
+    @DataSet(value = "datasets/yml/partial-user-dataset.yml", useSequenceFiltering = true)
     public void shouldSeedDataSetUsingSequenceFilter() {
         User user = (User) emProvider.em().createQuery("select u from User u where u.id = 1").getSingleResult();
         assertThat(user).isNotNull();
