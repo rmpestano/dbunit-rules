7770b9fda97617e774f819eadbe0f8be46b5aed7
rmpestano <bigboss666>
2015-07-26 23:45:23 -0300

    dbunit interceptor

---
+++ cdi/src/main/java/com/github/dbunit/rules/cdi/DBUnitInterceptor.java
@@ -0,0 +1,64 @@
+package com.github.dbunit.rules.cdi;
+
+import com.github.dbunit.rules.connection.ConnectionHolder;
+import com.github.dbunit.rules.connection.ConnectionHolderImpl;
+import com.github.dbunit.rules.dataset.DataSet;
+import com.github.dbunit.rules.dataset.DataSetExecutor;
+import com.github.dbunit.rules.dataset.DataSetModel;
+import org.hibernate.Session;
+import org.hibernate.internal.SessionImpl;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.sql.Connection;
+
+/**
+ * Created by pestano on 26/07/15.
+ */
+@Interceptor
+@DataSetInterceptor
+public class DBUnitInterceptor implements Serializable {
+
+    @Inject
+    EntityManager entityManager;
+
+    @AroundInvoke
+    public Object intercept(InvocationContext invocationContext)
+            throws Exception {
+
+        if (invocationContext.getMethod().getAnnotation(DataSet.class) != null) {
+            DataSet dataSet = invocationContext.getMethod().getAnnotation(DataSet.class);
+            if (dataSet != null) {
+                if (entityManager == null) {
+                    throw new RuntimeException("Provide entityManager instance via CDI @Producer in order to use DBUnitInterceptor");
+                }
+                Connection connection = null;
+                if (entityManager.getDelegate() instanceof Session) {
+                    connection = ((SessionImpl) entityManager.unwrap(Session.class)).connection();
+                } else {
+                    /**
+                     * see here:http://wiki.eclipse.org/EclipseLink/Examples/JPA/EMAPI#Getting_a_JDBC_Connection_from_an_EntityManager
+                     */
+                    entityManager.getTransaction().begin();
+                    connection = entityManager.unwrap(java.sql.Connection.class);
+                    entityManager.getTransaction().commit();
+                }
+
+                if (connection == null) {
+                    throw new RuntimeException("Could not get jdbc connection from provided entityManager");
+                }
+                ConnectionHolder connectionHolder = new ConnectionHolderImpl(connection);
+                DataSetModel dataSetModel = new DataSetModel().from(dataSet);
+                DataSetExecutor.instance(connectionHolder).execute(dataSetModel);
+            }
+        }
+
+        return invocationContext.proceed();
+    }
+
+
+}
---
+++ cdi/src/main/java/com/github/dbunit/rules/cdi/DataSetInterceptor.java
@@ -0,0 +1,15 @@
+package com.github.dbunit.rules.cdi;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.*;
+
+/**
+ * Created by pestano on 26/07/15.
+ */
+@Inherited
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface DataSetInterceptor {
+
+}
