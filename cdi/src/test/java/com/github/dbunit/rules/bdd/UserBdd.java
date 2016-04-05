1ed9638968e2b5246dc25c3d82dd172b70afb7b4
rmpestano <bigboss666>
2016-04-04 23:09:46 -0300

    configured cukedoctor

--- cdi/src/test/java/com/github/dbunit/rules/bdd/UserBdd.java
+++ cdi/src/test/java/com/github/dbunit/rules/bdd/UserBdd.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
  * Created by rafael-pestano on 09/10/2015.
  */
 @RunWith(CdiCucumberTestRunner.class)
-@CucumberOptions(features = "src/test/resources/features/find-users.feature")
+@CucumberOptions(features = "src/test/resources/features/find-users.feature", plugin = "json:target/dbunit.json")
 public class UserBdd {
 }
