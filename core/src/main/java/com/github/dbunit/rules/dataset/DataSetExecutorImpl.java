030d2adcde3c907746d4c37e728096209d4f1903
rmpestano <bigboss666>
2016-06-19 11:15:20 -0300

    fixes #26

--- core/src/main/java/com/github/dbunit/rules/dataset/DataSetExecutorImpl.java
+++ core/src/main/java/com/github/dbunit/rules/dataset/DataSetExecutorImpl.java
@@ -1,20 +1,13 @@
 package com.github.dbunit.rules.dataset;
 
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import com.github.dbunit.rules.api.dataset.*;
+import com.github.dbunit.rules.api.connection.ConnectionHolder;
+import com.github.dbunit.rules.api.dataset.DataSetExecutor;
+import com.github.dbunit.rules.api.dataset.DataSetModel;
+import com.github.dbunit.rules.api.dataset.JSONDataSet;
+import com.github.dbunit.rules.api.dataset.YamlDataSet;
 import com.github.dbunit.rules.assertion.DataSetAssertion;
 import com.github.dbunit.rules.exception.DataBaseSeedingException;
+import com.github.dbunit.rules.replacer.DateTimeReplacer;
 import com.github.dbunit.rules.replacer.ScriptReplacer;
 import org.dbunit.DatabaseUnitException;
 import org.dbunit.database.AmbiguousTableNameException;
@@ -31,8 +24,20 @@ import org.dbunit.operation.DatabaseOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.dbunit.rules.api.connection.ConnectionHolder;
-import com.github.dbunit.rules.replacer.DateTimeReplacer;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by pestano on 26/07/15.
@@ -108,7 +113,7 @@ public class DataSetExecutorImpl implements DataSetExecutor {
                     }
                 }
 
-                if(dataSetModel.getName() != null && !"".equals(dataSetModel.getName())){
+                if (dataSetModel.getName() != null && !"".equals(dataSetModel.getName())) {
                     IDataSet resultingDataSet = loadDataSet(dataSetModel.getName());
 
                     resultingDataSet = performSequenceFiltering(dataSetModel, resultingDataSet);
@@ -123,7 +128,7 @@ public class DataSetExecutorImpl implements DataSetExecutor {
                 }
 
             } catch (Exception e) {
-                throw new DataBaseSeedingException("Could not initialize dataset: "+dataSetModel, e);
+                throw new DataBaseSeedingException("Could not initialize dataset: " + dataSetModel, e);
             }
 
 
@@ -313,22 +318,25 @@ public class DataSetExecutorImpl implements DataSetExecutor {
      */
     public void clearDatabase(DataSetModel dataset) throws SQLException {
         Connection connection = connectionHolder.getConnection();
-        if (isHSqlDB()) {
-            connection.createStatement().execute("TRUNCATE SCHEMA public AND COMMIT;");
-        } else {
-
-            if (dataset != null && dataset.getTableOrdering() != null && dataset.getTableOrdering().length > 0) {
-                for (String table : dataset.getTableOrdering()) {
-                    connection.createStatement().executeUpdate("DELETE FROM " + table + " where 1=1");
-                    connection.commit();
+        if (dataset != null && dataset.getTableOrdering() != null && dataset.getTableOrdering().length > 0) {
+            for (String table : dataset.getTableOrdering()) {
+                if (table.toUpperCase().contains("SEQ")) {
+                    //tables containing 'SEQ'will NOT be cleared see https://github.com/rmpestano/dbunit-rules/issues/26
+                    continue;
                 }
-            }
-            //clear remaining tables in any order(if there are any, also no problem clearing again)
-            List<String> tables = getTableNames(connection);
-            for (String tableName : tables) {
-                connection.createStatement().executeUpdate("DELETE FROM " + tableName + " where 1=1");
+                connection.createStatement().executeUpdate("DELETE FROM " + table + " where 1=1");
                 connection.commit();
             }
+        }
+        //clear remaining tables in any order(if there are any, also no problem clearing again)
+        List<String> tables = getTableNames(connection);
+        for (String tableName : tables) {
+            if (tableName.toUpperCase().contains("SEQ")) {
+                //tables containing 'SEQ' will NOT be cleared see https://github.com/rmpestano/dbunit-rules/issues/26
+                continue;
+            }
+            connection.createStatement().executeUpdate("DELETE FROM " + tableName + " where 1=1");
+            connection.commit();
         }
 
     }
