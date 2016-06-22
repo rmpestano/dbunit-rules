486d6090deade341007cecef0f8222d8e274fa8a
rmpestano <rmpestano@gmail.com>
2016-06-22 16:38:53 -0300

    refactored to make cleanBeforeAfter test cleaner/simple

--- cdi/src/test/java/com/github/dbunit/rules/BeforeAndAfterTest.java
+++ cdi/src/test/java/com/github/dbunit/rules/BeforeAndAfterTest.java
@@ -5,6 +5,7 @@ import com.github.dbunit.rules.cdi.api.UsingDataSet;
 import com.github.dbunit.rules.model.User;
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +13,11 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
 
 /**
  * Created by pestano on 09/10/15.
@@ -24,35 +28,37 @@ public class BeforeAndAfterTest {
     @Inject
     EntityManager em;
 
+
     @Before
     public void init() {
         em.getTransaction().begin();
-        em.createNativeQuery("INSERT INTO USER VALUES (6,'user6')").executeUpdate();
+        em.createNativeQuery("DELETE FROM USER").executeUpdate();//delete users inserted in other tests
+        for (int i=0;i<6;i++ ) {
+            User u = new User();
+            u.setName("user" + i);
+            em.persist(u);
+        }//create 6 users
         em.flush();
         em.getTransaction().commit();
-        assertNotNull(getUser(6));
+        assertThat(listUsers()).isNotNull().hasSize(6);//we should have 6 users before test
     }
 
     @Test
     @UsingDataSet(value = "yml/users.yml", seedStrategy = SeedStrategy.INSERT,
-             cleanBefore = true, cleanAfter = true
+              cleanBefore = true, cleanAfter = true
     )
     public void shouldClearDatabaseBeforeAndAfter() {
-        try {
-            assertNull(getUser(6));
-            fail();//should not get here
-        }catch (NoResultException nre){
-            //no op
-        }
-        assertNotNull(getUser(1));//inserted by dbunit, see users.yml
+        //six users in @before must be deleted by cleanBefore
+        assertThat(listUsers()).
+                isNotNull().hasSize(2);//inserted by dbunit, see users.yml
     }
 
-    public User getUser(int id) {
-        return em.createQuery("select u from User u where u.id = " + id, User.class).getSingleResult();
+    public List<User> listUsers() {
+        return em.createQuery("select u from User u").getResultList();
     }
 
     @After
     public void end() {//clean after must delete rows inserted by dbunit
-        assertTrue(em.createNativeQuery("select * from USER u").getResultList().isEmpty());
+        assertThat(listUsers()).isEmpty();
     }
 }
